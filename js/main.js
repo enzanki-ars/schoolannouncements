f5c464d8935b98570975e613d1e8a22b45bb5edd
Alex Shafer <shafer.alex@gmail.com>
2016-08-17 13:44:33 -0400

    Start work on migration to Bootstrap v4-alpha

--- js/main.js
+++ js/main.js
@@ -1,10 +1,5 @@
-$(window).load(function() {
-  jQuery("time.timeago").timeago();
-  jQuery.timeago.settings.allowFuture = true;
-  $('[data-toggle="tooltip"]').tooltip();
-});
 function loadSuccess() {
-  document.getElementById("blockedInfo").innerHTML = "<span class=\"label label-info\">" 
+  document.getElementById("blockedInfo").innerHTML = "<span class=\"tag tag-info\">" 
 	+ "YouTube Access Check Succeeded.</span>";
   var image_x = document.getElementById('blockedImage');
   image_x.parentNode.removeChild(image_x);
@@ -20,14 +15,10 @@ function loadError() {
   image_x.parentNode.removeChild(image_x);
   ga('send', 'event', "YouTube Error", "YouTube Load Error");
 }
-$(document).ready(function() {
+$(window).on("load", function() {
+  jQuery("time.timeago").timeago();
+  jQuery.timeago.settings.allowFuture = true;
+  $('[data-toggle="tooltip"]').tooltip();
+  $('.dropdown-toggle').dropdown()
   $(".marquee").css("animation-duration", ($(window).width() + $(".marquee").width()) / 30 + "s")
-  if (window.location.protocol != "https:") {
-	document.getElementById('sitealerts').innerHTML +=
-	  "<div class=\"alert alert-danger\" role=\"alert\"><strong>DANGER:</strong> You are accessing this site over an "
-	  + "unsecured connection. Please use "
-	  + "<a href=\"{{ site.github.url | replace_first:'http://','https://' }}\" class=\"alert-link\">this link</a> "
-	  + "to go to the secured website.</div>";
-	ga('send', 'event', "HTTPS Error", "Page loaded on unsecure connection");
-  }
 });

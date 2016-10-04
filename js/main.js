cdaafd287a064cfcf89351a46f0cc17679b8950b
Alex Shafer <shafer.alex@gmail.com>
2016-10-04 13:24:45 -0400

    Fix some remaining merged files.

--- js/main.js
+++ js/main.js
@@ -2,15 +2,9 @@
 ---
 
 function loadSuccess() {
-<<<<<<<
-  document.getElementById("blockedInfo").innerHTML = "<span class=\"tag tag-info\">" 
-	+ "YouTube Access Check Succeeded.</span>";
-  var image_x = document.getElementById('blockedImage');
-=======
   document.getElementById("blockedInfo").innerHTML = "<span class=\"tag tag-info\">"
   + "YouTube Access Check Succeeded.</span>";
   var image_x = document.getElementById("blockedImage");
->>>>>>>
   image_x.parentNode.removeChild(image_x);
   ga("send", "event", "YouTube Success", "YouTube Load Success");
 }
